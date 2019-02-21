21fc45e726bfac2248d4286beb5ccc670029dfbd
hqtrung <haquangtrungceo@gmail.com>
2019-02-21 11:11:18 +0700

    add project structure

---
+++ TestingSystem.Data/TestingSystemEntities.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity;
+using TestingSystem.Data.Infrastructure;
+using TestingSystem.Models;
+
+namespace TestingSystem.Data
+{
+    class TestingSystemEntities:DbContext,IUnitOfWork
+    {
+        public TestingSystemEntities() : base("TestingSystem")
+        {
+        }
+       
+
+        public virtual void Commit()
+        {
+            base.SaveChanges();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Configtion
+
+        }
+
+
+
+
+
+
+        public DbSet<Group> Groups { get; set; }
+
+
+    }
+}
---
+++ TestingSystem.Models/Group.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestingSystem.Models
+{
+    public class Group
+    {
+        [Key]
+        public int GroupId { get; set; }
+
+        [Required(ErrorMessage = "Is Required")]
+        [MaxLength(50,ErrorMessage = ">50")]
+        public string GroupName { get; set; }
+
+        [Required(ErrorMessage = "Is Required")]
+        public DateTime CreatedDate { get; set; }
+
+        [Required(ErrorMessage = "Is Required")]
+        public DateTime UpdatedDate { get; set; }
+
+        public string Description { get; set; }
+    }
+}
